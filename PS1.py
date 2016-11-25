2e0e161e1879a2a70b8f321393b91e2e7c1b3f6b
SimonMartineau <simon.peter.martineau@gmail.com>
2016-11-25 10:37:21 -0500

    Update PS1.py

--- PS1.py
+++ PS1.py
@@ -1,3 +1,19 @@
 # Problem Set 1
 # Name: Simon Martineau
 # Time Spent:
+# Section a:
+
+
+
+# Problem Set 1
+# Name: Simon Martineau
+# Time Spent:
+# Section b:
+
+
+
+# Problem Set 1
+# Name: Simon Martineau
+# Time Spent:
+# Section c:
+
