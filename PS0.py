6c2548393ee1a394f5fa7a59d59c59b69e44237a
SimonMartineau <simon.peter.martineau@gmail.com>
2016-11-25 10:21:51 -0500

    created ps0.py
    This is the first problem set after lecture 1

---
+++ PS0.py
@@ -0,0 +1,6 @@
+# Problem Set 0 
+# Name: Simon Martineau
+# Time Spent: 0:15
+date_of_birth = input('Enter your date of birth:')
+name = input('Enter your name:')
+print(name + ' ' + date_of_birth)
