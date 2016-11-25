3f5442629e6e017d8e8a17971fe7794190967f3a
SimonMartineau <simon.peter.martineau@gmail.com>
2016-11-25 14:47:30 -0500

    Updates PS1.py
    Added link, Made changes to variable spelling (anual -> annual). Completed all problems.

--- PS1.py
+++ PS1.py
@@ -1,3 +1,4 @@
+# https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-00sc-introduction-to-computer-science-and-programming-spring-2011/unit-1/lecture-4-machine-interpretation-of-a-program/MIT6_00SCS11_ps1.pdf
 # Problem Set 1
 # Name: Simon Martineau
 # Time Spent: 0:45
