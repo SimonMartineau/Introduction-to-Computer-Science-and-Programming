982320dc174db443a85c87d1160729b70610b5e1
SimonMartineau <simon.peter.martineau@gmail.com>
2016-11-28 08:43:04 -0500

    created PS2_NEWTON file
    set layout

---
+++ PS2_NEWTON.py
@@ -0,0 +1,12 @@
+#https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-00sc-introduction-to-computer-science-and-programming-spring-2011/unit-1/lecture-6-recursion/MIT6_00SCS11_ps2.pdf
+# Problem Set 2a
+# Name: Simon Martineau
+# Time: 
+
+# Problem Set 2b
+# Name: Simon Martineau
+# Time: 
+
+# Problem Set 2c
+# Name: Simon Martineau
+# Time: 
